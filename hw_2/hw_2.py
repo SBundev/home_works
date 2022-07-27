8247010e11143239390405c047da9ee5ff1c90f5
sbundev <sbundev@hotmail.com>
2022-07-27 20:06:09 +0300

    home_work_2

---
+++ hw_2/hw_2.py
@@ -0,0 +1,23 @@
+#задание №1#
+var1 = 4
+var2 = 8
+var3 = var2
+var2 = var1
+var1 = var3
+print("var1 =",var1)
+print("var2 =", var2)
+
+#задание №2#
+var1 = 12
+var2 = 15
+var1, var2 = var2, var1
+print("var1 =",var1)
+print("var2 =", var2)
+#задание №3#
+var1 = 7
+var2 = 25
+var1 = var1 + var2
+var2 = var1 - var2
+var1 = var1 - var2
+print("var1 =",var1)
+print("var2 =", var2)
