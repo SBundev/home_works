d3efbbe927337805fa0430b149632690990e26e9
sbundev <sbundev@hotmail.com>
2022-07-31 08:44:00 +0300

    hw_4_2

---
+++ hw_4/hw_4_2.py
@@ -0,0 +1,18 @@
+clas1 = input("Укажите количество учеников в классе: ")
+var1 = float(clas1)
+clas2 = input("Укажите количество учеников в классе: ")
+var2 = float(clas2)
+clas3 = input("Укажите количество учеников в классе: ")
+var3 = float(clas3)
+var1_1 = float(var1 // 2)
+var1_2 = float(var1 % 2)
+var1 = var1_1 + var1_2
+print("нужно купить парт в первый класс: ", + var1)
+var2_1 = float(var2 // 2)
+var2_2 = float(var2 % 2)
+var2 = var2_1 + var2_2
+print("нужно купить парт во второй класс: ", + var2)
+var3_1 = float(var3 // 2)
+var3_2 = float(var3 % 2)
+var3 = var3_1 + var3_2
+print("нужно купить парт в третий класс: ", + var3)
