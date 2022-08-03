28a26b953d5e372bc9f3edfdcd621435681b8575
sbundev <sbundev@hotmail.com>
2022-08-03 18:35:09 +0300

    hw_5_3

---
+++ hw_5/hw_5_3.py
@@ -0,0 +1,34 @@
+count_1 = 0  # К-во четных чисел
+count_2 = 0  # К-во не четных чисел
+count_3 = 0  # К-во введенных чисел
+min = 500 ** 500
+max = 0
+summ = 0
+mid = 0
+i = 0
+while True:
+    n = int(input("""Введите любые натрульне числа,
+    окончанием ввода является 0 : """))
+    if n == 0:
+        break
+    elif n % 2 == 0:
+        count_1 += 1
+    elif n % 1 == 0:
+        count_2 += 1
+    if n > max :
+        max = n
+    if n < min:
+        min = n
+    if n != summ or n == summ:
+        summ = summ + n
+    if n % 2 == 0 and n % 1 == 0:
+        count_3 += 1
+        i = count_3 - 1
+        mid = summ / count_3
+        print(summ)
+print("К-во четных чисел :", count_1)
+print("К-во не четных чисел :", count_2)
+print("Максимальное введеное число : ", max)
+print("Минимальное веденное число : ", min)
+print("Сумма всех введеных чисел :", summ)
+print("Средне арифметическое всех чисел :", mid)
