0ba4c487f1bd2fb7267a9d48a3c880bb0d2e1867
sbundev <sbundev@hotmail.com>
2022-08-03 20:36:40 +0300

    hw_5_3

--- hw_5/hw_5_3.py
+++ hw_5/hw_5_3.py
@@ -5,7 +5,6 @@ min = 500 ** 500
 max = 0
 summ = 0
 mid = float(0)
-i = 0
 while True:
     n = int(input("""Введите любые натрульне числа,
     окончанием ввода является 0 : """))
@@ -21,10 +20,9 @@ while True:
         min = n
     if n != summ or n == summ:
         summ = summ + n
-    if n % 2 == 0 and n % 1 == 0:
-        i += 1
-        count_3 = i + 1
-        mid = float(summ // count_3)
+    if n % 2 == 0 or n % 1 == 0:
+        count_3 += 1
+        mid = summ / count_3
 print("К-во четных чисел :", count_1)
 print("К-во не четных чисел :", count_2)
 print("К-во введеных чисел :", count_3)
