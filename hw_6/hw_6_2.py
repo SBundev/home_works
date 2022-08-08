7c00a76e7c628f614280eb9971827b8284e8590f
sbundev <sbundev@hotmail.com>
2022-08-08 22:09:49 +0300

    hw_6_2

--- hw_6/hw_6_2.py
+++ hw_6/hw_6_2.py
@@ -1,12 +1,22 @@
 var_string = input("Введите текст: ")
 var_string = var_string.lower()
 var_char = input("Введите символ: ")
-count = 0
-#print("Количество символов в введенной строке =", var_string.find(var_char))
+count_1 = 0
+ #  Вариант №1
 for i in var_string:
     if i == var_char:
-        count += 1
+        count_1 += 1
 
-print("К-во введенных символ в тексте = ", count)
-#    elif i != var_char:
-#        print("Текст не садержит введенного символа")
+print("К-во введенных символов в строке = ", count_1)
+
+ #  Вариант №2
+
+var = -1
+count_2 = 0
+
+while True:
+    var = var_string.find(var_char, var + 1)
+    if var == -1:
+        break
+    count_2 += 1
+print("К-во введенных символов  в строке =", count_2)
