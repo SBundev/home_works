8f2dbd433b1a27c529c6574aaaa47f323b985104
sbundev <sbundev@hotmail.com>
2022-08-08 23:14:30 +0300

    hw_6_2

--- hw_6/hw_6_3.py
+++ hw_6/hw_6_3.py
@@ -1,10 +1,10 @@
 x = int(input("Введите число от 3 до 9 : "))
-string = ('123456789')
+s = int(123456789)
 
 while True:
     if x < 3 or x > 9:
         print("Введено не верное число")
         break
-    for i in range(string[:x-1]):
+    for i in range(10):
         if i == 2:
-            print("1,2,1")
+            print("121")
