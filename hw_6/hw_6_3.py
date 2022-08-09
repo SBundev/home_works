a25b949b7d0ac56d3314a05ed99fb8093c4d2146
sbundev <sbundev@hotmail.com>
2022-08-09 19:32:42 +0300

    hw_6_3

--- hw_6/hw_6_3.py
+++ hw_6/hw_6_3.py
@@ -1,10 +1,17 @@
-x = int(input("Введите число от 3 до 9 : "))
-s = int(123456789)
-
+x = input("Введите число от 3 до 9 : ")
 while True:
-    if x < 3 or x > 9:
+    if not x.isdigit():
+        print("Введено не число")
+        break
+    elif   int(x)< 3 or int(x) > 9:
         print("Введено не верное число")
         break
-    for i in range(10):
-        if i == 2:
-            print("121")
+    else:
+        c = int(x)
+        for i in range(1, c+1):
+            for k in range(1, i+1):
+                print(k, end=" ")
+            for k in range(i-1, 0, -1):
+                print(k, end=" ")
+            print()
+        break
