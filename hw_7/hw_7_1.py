bf6fdedb39df51334b865950d2312056f65f99d4
sbundev <sbundev@hotmail.com>
2022-08-10 20:21:53 +0300

    hw_7_1

--- hw_7/hw_7_1.py
+++ hw_7/hw_7_1.py
@@ -1,5 +1,6 @@
-lst = list(input("Введите список состоящий из чисел : "))
+lst = list(input("Введите список состоящий из чисел : ").split())
 k = int(input("Введите индекс элемента из списка : "))
-lst[::] = lst[:k-1] + lst[k:] + lst[k-1:k]
+for n in range(k,len(lst) - 1):
+    lst[n] = lst[n+1]
 lst.pop()
 print(lst)
