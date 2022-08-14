59a5e3ef4359ba2eb606fb0651f7e1b9efc5326c
sbundev <sbundev@hotmail.com>
2022-08-14 15:29:38 +0300

    hw_8_1

--- hw_8/hw_8_1.py
+++ hw_8/hw_8_1.py
@@ -1,4 +1,4 @@
 n = int(input("Введите размер матрици: "))
 a = [[i if j % 2 == 1 else j for i in range(-n,0)] for j in range( n)]
-for row in a:
-    print(' '.join([str(elem) for elem in row]))
+for b in a:
+    print(b)
