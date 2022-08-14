1b1db025891ef952648cbda7a1059548d1ffadeb
sbundev <sbundev@hotmail.com>
2022-08-14 16:22:05 +0300

    hw_8_3

---
+++ hw_8/hw_8_3.py
@@ -0,0 +1,10 @@
+import random
+a = [random.randint(-100, 100) for i in range(15)]
+b = sum(i if i % 2 == 0 else 0 for i in a )
+c = sum(i if i % 2 != 0 else 0 for i in a )
+if b > c:
+    print("да")
+else:
+    print("Нет")
+#print(a,b,c)
+
