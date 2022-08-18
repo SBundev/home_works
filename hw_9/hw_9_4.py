360641b270c40f9b7f8e4b5bcacb58698a50eab7
sbundev <sbundev@hotmail.com>
2022-08-18 18:34:07 +0300

    hw_9_4

--- hw_9/hw_9_4.py
+++ hw_9/hw_9_4.py
@@ -1,6 +1,6 @@
 import random
-n = [random.randint(1, 50) for i in range(0,10)]
-c = [random.randint(50, 99) for c in range(0,10)]
+n = [random.randint(1, 50) for i in range(0, 10)]
+r = [random.randint(50, 99) for c in range(0, 10)]
 print(type(n))
-dct = {k: v for k, v in zip(n, c)}
+dct = {k: v for k, v in zip(n, r)}
 print(dct)
