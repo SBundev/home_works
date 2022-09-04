f06c6f5293f24fd630b31831d59b2da059634cea
sbundev <sbundev@hotmail.com>
2022-09-04 22:05:50 +0300

    hw_14_1

--- hw_14/hw_14_1.py
+++ hw_14/hw_14_1.py
@@ -1,9 +1,9 @@
 from random import randint
-from sorti import p
+from sorti import sorti_bubble
 
 
 if __name__ == "__main__":
     n = int(input("Введите длину списка: "))
     strim = [randint(1, 50) for i in range(n)]
-    sorti_insertion(strim)
+    sorti_bubble(strim)
     print(strim)
