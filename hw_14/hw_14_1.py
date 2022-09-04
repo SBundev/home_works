99db2faa7da2aea9c95a6d12e4d120f98841af68
sbundev <sbundev@hotmail.com>
2022-09-04 19:50:43 +0300

    hw_14_1

--- hw_14/hw_14_1.py
+++ hw_14/hw_14_1.py
@@ -1,5 +1,9 @@
 from random import randint
+from sorti import sorti_quick
+
 
 if __name__ == "__main__":
-    n = int(input("Введите количество рядов матриц: "))
-    strim = [[randint(1, 50) for i in range(n)] for j in range(m)]
+    n = int(input("Введите длину списка: "))
+    strim = [[randint(1, 50) for i in range(n)]]
+    sorti_quick(strim)
+    print(strim)
--- hw_14/sorti.py
+++ hw_14/sorti.py
@@ -16,28 +16,38 @@
     Далее делит каждый из подсписков на по новому опорному элементу и
     алгоритм повторяется
 """
-def partision(nums, low, high):
+
+
+def partition(nums, low, high):
     pivot = nums[(low + high) // 2]
     i = low - 1
-    j = high +1
+    j = high + 1
     while True:
         i += 1
         while nums[i] < pivot:
             i += 1
 
-        j += 1
+        j -= 1
         while nums[j] > pivot:
-            j += 1
+            j -= 1
 
         if i >= j:
             return j
         nums[i], nums[j] = nums[j], nums[i]
 
+
 def sorti_quick(nums):
     def _sorti_quick(item, low, high):
         if low < high:
-            split_index = partision(item, low, high)
-            _sorti_quick(item,low,split_index)
+            split_index = partition(item, low, high)
+            #print(nums)
+            _sorti_quick(item, low, split_index)
             _sorti_quick(item, split_index + 1, high)
 
     _sorti_quick(nums, 0, len(nums) - 1)
+
+
+if __name__ == "__main__":
+    lst = [12, 15, 97, 64, 15, 97, 5496, 125, 84, 31, 17]
+    sorti_quick(lst)
+    print(lst)
